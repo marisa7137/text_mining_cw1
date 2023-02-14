5f8d09e7d962e123e255f561cd79508443b45a16
XiaofeiChang <953702362@qq.com>
2023-02-14 15:31:17 +0000

    added some useful code

--- src/text_parser.py
+++ src/text_parser.py
@@ -5,8 +5,8 @@ import torch
 
 class TextParser:
     def __init__(self, config=None):
-        self.raw_text_path = '../data/train_5500.label.txt'  # Path for the raw text
-        self.stopwords_path = '../data/stopwords.txt'  # Path for the stopwords collection text
+        self.raw_text_path = './data/train_5500.label.txt'  # Path for the raw text
+        self.stopwords_path = './data/stopwords.txt'  # Path for the stopwords collection text
         self.training_set_path = ''  # Path for the split training set
         self.dev_set_path = ''  # Path for the developing set
         self.labels_path = ''  # Path for the label collection
