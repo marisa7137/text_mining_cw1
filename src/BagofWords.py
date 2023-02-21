317aa4e2f321b10d2f0bd6f97620070137bddf82
xuting <xuting20000229@163.com>
2023-02-21 14:22:58 +0000

    update BOW training

--- src/BagofWords.py
+++ src/BagofWords.py
@@ -1,12 +1,34 @@
 import torch
-class BagOfWords(torch.nn.Module):
-    def __init__(self, vocab_size):
-        super().__init__()
-        self.vocab_size = vocab_size
-        self.embedding = torch.nn.EmbeddingBag(vocab_size, 10)
-        self.fc = torch.nn.Linear(10, 4)
+import torch.nn as nn
+from word_embedding import Word_Embedding
+from sentence_rep import Sentence_Rep
 
-    def forward(self, text):
-        x = self.embedding(text, torch.zeros(text.shape, dtype=torch.long))
-        x = self.fc(x)
-        return x
+class Model(torch.nn.Module):
+    def __init__(self, pre_train_weight, vocab_size, embedding_dim, from_pre_train: bool, freeze: bool, bow: bool, hidden_dim_bilstm, hidden_layer_size, num_of_classes):
+        super().__init__()
+        
+        self.word_embedding = Word_Embedding(pre_train_weight=pre_train_weight, vocab_size=vocab_size, embedding_dim=embedding_dim, from_pre_train=from_pre_train, freeze=freeze)
+        self.sen_rep = Sentence_Rep(bow=bow, embedding_dim=embedding_dim, hidden_dim_bilstm=hidden_dim_bilstm)
+        self.fc1 = nn.Linear(in_features=embedding_dim, out_features=hidden_layer_size)
+        self.af1 = nn.LeakyReLU(0.1)
+        self.fc2 = nn.Linear(in_features=hidden_layer_size, out_features=num_of_classes)
+        self.af2 = nn.LogSoftmax(dim=0)
+       
+
+
+    def forward(self, indexed_sentence):
+        out = self.word_embedding(indexed_sentence)
+        #print(out)
+        #print(out.shape) #shape:torch.Size([545, 18, 30])
+        out = self.sen_rep(out)
+        #print("sen_rep:",out.shape) 
+        out = self.fc1(out)
+        #print("fc1 size",out.shape)
+        out = self.af1(out)
+        #print("af1 size",out.shape)
+        out = self.fc2(out)
+        #print("fc2 size",out.shape)
+        out = self.af2(out)
+        #print("af2 size",out.shape)
+        #print("torch mean:",torch.mean(out,dim=1))
+        return out
---
+++ src/BagofWords_train.py
@@ -0,0 +1,102 @@
+import numpy as np
+import torch
+from torch.optim.lr_scheduler import ExponentialLR
+from torch.utils.data.dataloader import DataLoader
+from text_parser import TextParser
+#from bilstm import Model
+from BagofWords import Model
+import torch.optim as optim
+from sklearn.metrics import accuracy_score, f1_score
+from configparser import ConfigParser
+# import the configure files
+config = ConfigParser()
+config.read("src/BagofWords.config")
+
+
+
+
+def train(t, train_data, num_classes):
+    '''
+            The main function for testing
+    '''
+
+    lr = 7e-2
+    epochs = 10
+    batch_size = 545
+
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+
+    model = Model(pre_train_weight=None, vocab_size=len(t.vocab), embedding_dim=30, from_pre_train=False, freeze=False,
+                    bow=True, hidden_dim_bilstm=20, hidden_layer_size=30, num_of_classes=num_classes)
+
+    loss_function = torch.nn.NLLLoss(reduction='mean') # calculate the average negative log loss of a batch
+
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    scheduler = ExponentialLR(optimizer, gamma=0.9)
+
+    losses, train_accs, train_F1s = [], [], []
+    model.train()
+    for epoch in range(epochs):
+        cnt = 0 # the number of batches
+        loss_batch = 0 # the sum of average loss of each batch within a epoch
+        acc_batch = 0 # the sum of average accuracy of each batch within a epoch
+        f1_batch = 0 # the sum of average f1 score of each batch within a epoch
+
+        for train_labels, train_features in iter(train_loader):
+            
+            train_labels = train_labels.type(torch.LongTensor) # shape (batch_size) <==> (500)
+            if len(train_labels) != batch_size:
+                continue
+            #print(model)
+            output = model(train_features) # shape (batch_size, class_num) <==> (500, 50)
+            #print("output size:",output.shape)
+
+            # compute loss
+            loss = loss_function(output, train_labels)  # calculate loss
+            loss_batch += float(loss)
+
+            # get the index of the class with the maximum likelihood
+            output_idx = torch.argmax(output, dim=1).cpu().data.numpy() # shape: (64,)
+
+            # accuracy and f1
+            acc = accuracy_score(output_idx, train_labels)
+            f1 = f1_score(output_idx, train_labels, average="micro")
+            acc_batch += acc
+            f1_batch += f1
+
+            # network propagation
+            optimizer.zero_grad()  # clean gradients
+            loss.backward()  # backward pass
+            optimizer.step()  # update weights
+
+            # print for each batch
+            cnt += 1
+            adam_lr = optimizer.param_groups[0]['lr']
+            # print("Train batch", f'epoch: {epoch}, batch: {cnt}, loss: {loss_batch}, accuracy: {acc}, f1 Score: {f1}, lr: {adam_lr}')
+
+        # record the data for each epoch
+        losses.append(loss_batch/cnt)  # average loss of the batch
+        train_accs.append(acc_batch/cnt)
+        train_F1s.append(f1_batch/cnt)
+        # print for each epoch
+        print("Train epoch", f'epoch: {epoch}, loss: {loss_batch/cnt}, accuracy: {acc_batch/cnt}, f1 Score: {f1_batch/cnt}, lr: {adam_lr}')
+
+        # update optimizer scheduler
+        scheduler.step()
+
+
+    # plot_history(accList, lossList "./")
+
+    # save the model
+    if num_classes == 6:
+        np.savetxt(config.get("param","loss_bow_coase"), losses)
+        np.savetxt(config.get("param","acc_bow_coase"), train_accs)
+        np.savetxt(config.get("param","f1_bow_coase"), train_F1s)
+        torch.save(model, config.get("param","bow_coase_pth"))
+        print("successfully saved the coase model!")
+    else:
+        np.savetxt(config.get("param","loss_bow_fine"), losses)
+        np.savetxt(config.get("param","acc_bow_fine"), train_accs)
+        np.savetxt(config.get("param","f1_bow_fine"), train_F1s)
+        torch.save(model, config.get("param","bow_fine_pth"))
+        print("successfully saved the fine model!")
