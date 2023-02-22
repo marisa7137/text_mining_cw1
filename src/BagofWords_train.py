71b7cca44a398301571a921248fd02256c18e460
AdahTing <98388239+AdaTingAI@users.noreply.github.com>
2023-02-22 20:04:26 +0000

    Update BagofWords_train.py
    with development function

--- src/BagofWords_train.py
+++ src/BagofWords_train.py
@@ -3,7 +3,6 @@ import torch
 from torch.optim.lr_scheduler import ExponentialLR
 from torch.utils.data.dataloader import DataLoader
 from text_parser import TextParser
-#from bilstm import Model
 from BagofWords import Model
 import torch.optim as optim
 from sklearn.metrics import accuracy_score, f1_score
@@ -12,12 +11,56 @@ from configparser import ConfigParser
 config = ConfigParser()
 config.read("src/BagofWords.config")
 
-
-
-
-def train(t, train_data, num_classes):
+def development(batch_size, dev_loader, model, loss_function, dev_losses, dev_accs, dev_F1s):
     '''
-            The main function for testing
+    The function for model development (fine-tuning/optimisation) at the end of each epoch
+    :param int batch_size: the developing batch size
+    :param DataLoader dev_loader: the developing data loader
+    :param Model model: the model updated in the current epoch
+    :param torch.nn.NLLLoss loss_function: the loss function
+    :param list dev_losses: the developing loss list
+    :param list dev_accs: the developing accuracy list
+    :param list dev_F1s: the developing F1 score list
+    :return: dev_losses, dev_accs, dev_F1s
+    '''
+
+    # turn off gradients computation
+    with torch.no_grad():
+        for dev_labels, dev_features in iter(dev_loader):
+            # convert the labels to tensor
+            dev_labels = dev_labels.type(torch.LongTensor)  # shape (batch_size,) <==> (545,)
+
+            # to ensure the word embedding work correctly
+            if len(dev_labels) != batch_size:
+                break
+
+            # prediction
+            output = model(dev_features)
+
+            # calculate loss
+            loss = loss_function(output, dev_labels)
+            dev_losses.append(float(loss))
+
+            # get the index of the class with the maximum likelihood
+            output_idx = torch.argmax(output, dim=1).cpu().data.numpy()  # shape: (545,)
+
+            # calculate accuracy and f1
+            acc = accuracy_score(output_idx, dev_labels)
+            f1 = f1_score(output_idx, dev_labels, average="micro")
+            dev_accs.append(acc)
+            dev_F1s.append(f1)
+
+    return dev_losses, dev_accs, dev_F1s
+
+
+def train(t, train_data, dev_data, num_classes):
+    '''
+    The main function for training
+    :param TextParser t: test parser
+    :param list train_data: the training data
+    :param list dev_data: the developing data
+    :param int num_classes: the number of classes, 6 or 50
+    :return: None
     '''
 
     lr = 2e-2
@@ -25,16 +68,20 @@ def train(t, train_data, num_classes):
     batch_size = 545
 
     train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    dev_loader = DataLoader(dev_data, batch_size=batch_size, shuffle=True)
 
-    model = Model(pre_train_weight=None, vocab_size=len(t.vocab), embedding_dim=300, from_pre_train=False, freeze=False,
-                    bow=True, hidden_dim_bilstm=200, hidden_layer_size=50, num_of_classes=num_classes)
+    model = Model(pre_train_weight=None, vocab_size=len(t.vocab), embedding_dim=400, from_pre_train=False, freeze=False,
+                    bow=True, hidden_dim_bilstm=300, hidden_layer_size=50, num_of_classes=num_classes)
 
     loss_function = torch.nn.NLLLoss(reduction='mean') # calculate the average negative log loss of a batch
-
+    
     optimizer = optim.Adam(model.parameters(), lr=lr)
     scheduler = ExponentialLR(optimizer, gamma=0.9)
 
-    losses, train_accs, train_F1s = [], [], []
+    # initialize the lists that record the results
+    train_losses, train_accs, train_F1s = [], [], []
+    dev_losses, dev_accs, dev_F1s = [], [], []
+
     model.train()
     for epoch in range(epochs):
         cnt = 0 # the number of batches
@@ -45,32 +92,28 @@ def train(t, train_data, num_classes):
         for train_labels, train_features in iter(train_loader):
             
             train_labels = train_labels.type(torch.LongTensor) # shape (batch_size) <==> (500)
+            
+            # to ensure the word embedding work correctly
             if len(train_labels) != batch_size:
                 continue
-            #print(model)
-            output = model(train_features) # shape (batch_size, class_num) <==> (500, 50)
-            #print("output size:",output.shape)
-            #print("output:",output)
-            #print("output size:",output.shape)
-            #print("train label:",train_labels)
-            #print("trainlabel size:",train_labels.shape)
 
-            # compute loss
+            # prediction
+            output = model(train_features) # shape (batch_size, class_num) <==> (500, 50)
+
+            # calculate loss
             loss = loss_function(output, train_labels)  # calculate loss
             loss_batch += float(loss)
 
             # get the index of the class with the maximum likelihood
             output_idx = torch.argmax(output, dim=1).cpu().data.numpy() # shape: (64,)
-            #print("output_idx:",output_idx)
-            #print("output_idx size:",output_idx.shape)
-
+        
             # accuracy and f1
             acc = accuracy_score(output_idx, train_labels)
             f1 = f1_score(output_idx, train_labels, average="micro")
             acc_batch += acc
             f1_batch += f1
 
-            # network propagation
+            # calculate the gradients and do back propagation
             optimizer.zero_grad()  # clean gradients
             loss.backward()  # backward pass
             optimizer.step()  # update weights
@@ -80,10 +123,17 @@ def train(t, train_data, num_classes):
             adam_lr = optimizer.param_groups[0]['lr']
             # print("Train batch", f'epoch: {epoch}, batch: {cnt}, loss: {loss_batch}, accuracy: {acc}, f1 Score: {f1}, lr: {adam_lr}')
 
+            # model development at the end of each epoch. There are 9 batches in each epoch
+            if cnt==9:
+                dev_losses, dev_accs, dev_F1s = development(batch_size, dev_loader, model, loss_function, dev_losses, dev_accs, dev_F1s)
+                print("Dev batch", f'epoch: {epoch}, loss: {dev_losses[-1]}, accuracy: {dev_accs[-1]}, f1 Score: {dev_F1s[-1]}')
+
+
         # record the data for each epoch
-        losses.append(loss_batch/cnt)  # average loss of the batch
+        train_losses.append(loss_batch/cnt)  # average loss of the batch
         train_accs.append(acc_batch/cnt)
         train_F1s.append(f1_batch/cnt)
+
         # print for each epoch
         print("Train epoch", f'epoch: {epoch}, loss: {loss_batch/cnt}, accuracy: {acc_batch/cnt}, f1 Score: {f1_batch/cnt}, lr: {adam_lr}')
 
@@ -95,14 +145,30 @@ def train(t, train_data, num_classes):
 
     # save the model
     if num_classes == 6:
-        np.savetxt(config.get("param","loss_bow_coase"), losses)
+        # save training records
+        np.savetxt(config.get("param","loss_bow_coase"), train_losses)
         np.savetxt(config.get("param","acc_bow_coase"), train_accs)
         np.savetxt(config.get("param","f1_bow_coase"), train_F1s)
+
+        # save developing records
+        np.savetxt(config.get("param","loss_bow_coase"), dev_losses)
+        np.savetxt(config.get("param","acc_bow_coase"), dev_accs)
+        np.savetxt(config.get("param","f1_bow_coase"), dev_F1s)
+
+        #save the model
         torch.save(model, config.get("param","bow_coase_pth"))
         print("successfully saved the coase model!")
     else:
-        np.savetxt(config.get("param","loss_bow_fine"), losses)
+        # save training records
+        np.savetxt(config.get("param","loss_bow_fine"), train_losses)
         np.savetxt(config.get("param","acc_bow_fine"), train_accs)
         np.savetxt(config.get("param","f1_bow_fine"), train_F1s)
+
+        # save developing records
+        np.savetxt(config.get("param","loss_bow_fine"), dev_losses)
+        np.savetxt(config.get("param","acc_bow_fine"), dev_accs)
+        np.savetxt(config.get("param","f1_bow_fine"), dev_F1s)
+
+        # save the model
         torch.save(model, config.get("param","bow_fine_pth"))
         print("successfully saved the fine model!")
